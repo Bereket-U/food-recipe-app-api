9ded1128b2eacf805697ec90c6f9fd9ab3595af8
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-21 14:49:36 -0400

    create getData function to make request and get data using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const APP_ID = "cbfb9ddd";
   const APP_KEY = "eaffffd5345dc91211b5f43a73e5975f";
+  const searchString = "pizza";
   const url = `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    const result = await axios.get(url);
+    console.log(result);
+  };
+
   return (
     <div className="App">
       <h1>Food Recipe App</h1>
