06a107252a2e969e27d0d9dd672e6c7ea47bb6d3
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-21 17:37:48 -0400

    import Ingredients component and pass recipe ingredients

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Ingredients from "../Ingredients/Ingredients";
 
 const Recipe = ({ recipe }) => {
   const { label, image, url, ingredients } = recipe.recipe;
+  const [display, setDisplay] = useState(false);
+  console.log(display);
+
   return (
     <div>
       <h3>{label}</h3>
@@ -9,7 +13,8 @@ const Recipe = ({ recipe }) => {
       <a href={url} target="_blank" rel="noopener noreferrer">
         Read More
       </a>
-      <button>Ingredients</button>
+      <button onClick={() => setDisplay(!display)}>Ingredients</button>
+      {display && <Ingredients ingredients={ingredients} />}
     </div>
   );
 };
