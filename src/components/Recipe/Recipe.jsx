f8fe0c4486a2cbcfc3b8f04df594dc6eb386d2e8
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-22 20:12:29 -0400

    style recipe component using MUI

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import Ingredients from "../Ingredients/Ingredients";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const Recipe = ({ recipe }) => {
   const { label, image, url, ingredients } = recipe.recipe;
@@ -10,15 +16,32 @@ const Recipe = ({ recipe }) => {
   };
 
   return (
-    <div>
-      <h3>{label}</h3>
-      <img src={image} alt="label" />
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        Read More
-      </a>
-      <button onClick={handleExpandClick}>Ingredients</button>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" alt={label} height="140" image={image} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {label}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {ingredients[0].text}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {ingredients[2].text}
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button size="small" onClick={handleExpandClick}>
+          Read More
+        </Button>
+        <form action={url}>
+          <Button type="submit" size="small">
+            link
+          </Button>
+        </form>
+      </CardActions>
       {expanded && <Ingredients ingredients={ingredients} />}
-    </div>
+    </Card>
   );
 };
 
