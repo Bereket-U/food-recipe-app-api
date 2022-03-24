e7c5e27a997377b32c7afa1746b17c24349bd9fb
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-24 14:42:57 -0400

    Add recipes component for all fillterd from api

---
+++ src/components/Recipes/Recipes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { v4 as uuid } from "uuid";
+import Box from "@mui/material/Box";
+import { Grid } from "@mui/material";
+import Recipe from "../Recipe/Recipe";
+
+const Recipes = ({ recipes }) => {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 3, md: 3 }}
+          direction="row"
+          justifyContent="center"
+        >
+          {recipes != [] &&
+            recipes.map((recipe) => (
+              <Grid item key={uuid()}>
+                <Recipe key={uuid()} recipe={recipe} />
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default Recipes;
