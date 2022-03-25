d34a3480452e50600105c379ae7999d57231a559
Bereket Urgessa <bereketadugna15@gmail.com>
2022-03-24 22:46:40 -0400

    update alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Alert as MyAlert } from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+// const Alert = ({ alert }) => {
+//   return (
+//     <div>
+//       {alert === "" ? (
+//         ""
+//       ) : (
+//         <Stack sx={{ minWidth: "40%" }} spacing={2}>
+//           <MyAlert severity="error">{alert}</MyAlert>
+//         </Stack>
+//       )}
+//     </div>
+//   );
+// };
 
 const Alert = ({ alert }) => {
-  return <div>{alert}</div>;
+  if (alert !== "") {
+    return (
+      <Stack
+        sx={{
+          minWidth: "40%",
+        }}
+        alignItems="center"
+        justifyContent="center"
+        alignItems="center"
+        marginTop={0}
+      >
+        <MyAlert severity="error">{alert}</MyAlert>
+      </Stack>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default Alert;
